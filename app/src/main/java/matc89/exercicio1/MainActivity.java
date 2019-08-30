d4b7118e6470a8e0053ab6f10ecaafdbd69c5564
Liliane Fiuza <liliane.fiuza@gmail.com>
2019-08-30 20:15:35 -0300

    ok

--- app/src/main/java/matc89/exercicio1/MainActivity.java
+++ app/src/main/java/matc89/exercicio1/MainActivity.java
@@ -2,12 +2,45 @@ package matc89.exercicio1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
+    private Button botao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.labelMensagem);
+        editText = (EditText) findViewById(R.id.editNome);
+        botao = (Button) findViewById(R.id.btnCumprimentar);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Alô," + " " + editText.getText().toString() + "!");
+            }
+        });
     }
+
+    protected void onSaveInstanceState(Bundle state){
+        super.onSaveInstanceState(state);
+        state.putString("texto", editText.getText().toString());
+
+    }
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+
+        textView.setText("Alô," + " " + savedInstanceState.getString("texto") + "!");
+    }
+
 }
